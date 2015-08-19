cdca69ed9ee0a379b20eb505638fb453338cca8a
Varun N R <nr.varun79@gmail.com>
2015-08-19 21:28:00 +0530

    first updation

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name profileMaterialApp
+ * @description
+ * # profileMaterialApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('profileMaterialApp', [ 'ngMaterial','ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+});
+app.controller('profileCtrl',['$scope','$mdSidenav','$mdBottomSheet',function($scope,$mdSidenav,$mdBottomSheet){
+    $scope.toggleLeft = function(){
+        $mdSidenav('left').toggle();
+    };
+
+    $scope.showBottomSheetSocial = function() {
+    $mdBottomSheet.show({
+          templateUrl: 'views/bottomSheets/bottomSheetSocialShare.html'
+    });
+  };
+}]);
---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name profileMaterialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the profileMaterialApp
+ */
+angular.module('profileMaterialApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
