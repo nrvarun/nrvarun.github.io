1aa8dbf3b25bf21ada264ceabdec7dfe3cc7f5a1
nrvarun <nr.varun79@gmail.com>
2016-06-18 01:13:44 +0530

    skills section updated

---
+++ scripts/controllers/skills.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name profileMaterialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the profileMaterialApp
+ */
+angular.module('profileMaterialApp')
+  .controller('SkillsCtrl', function () {
+  });
